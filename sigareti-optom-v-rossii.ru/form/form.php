db6371b0f58a36f9b9db20974bc2c02a22d31009
dashaOdinDvaTri <miron.dasha@gmail.com>
2020-09-20 18:52:10 +0300

    first commit

---
+++ sigareti-optom-v-rossii.ru/form/form.php
@@ -0,0 +1,42 @@
+<?php
+error_reporting(-1);
+header('Content-Type: text/html; charset=utf-8');
+
+$ini = parse_ini_file('../config/config.ini');
+
+$utmz = $_COOKIE['__utmz'];
+$utmz = explode(".", $utmz);
+$utmz = array_pop($utmz);
+$utmz = explode('|', $utmz);
+$utmz_out = array();
+foreach ($utmz as &$part) {
+	$part = explode('=', $part);
+	$utmz_out[$part[0]] = $part[1];
+}
+
+$mail_title = 'Заказ с сайта сигареты-оптом-в-россии.ру';
+
+if (isset($_POST['call'])) {
+	$mail_title = 'Заказ обратного звонка с сайта сигареты-оптом-в-россии.ру';
+}
+
+$mail = "
+	<html>
+		<head>
+		  <title>{$mail_title}</title>
+		</head>
+		<body style='color:#111; font-family:13px Tahoma, Arial, sans-serif;'>
+			<p><strong>{$mail_title}<strong></p>
+			<p>Имя: {$_POST['name']}</p>
+			<p>Телефон: {$_POST['phone']}</p>
+			<p>E-mail: {$_POST['email']}</p>
+			
+		</body>
+	</html>
+";
+
+$headers = 'Content-type: text/html; charset=utf-8' . "\r\n" .
+		'From: ' . $ini['company_name'] . ' <' . $ini['from_email'] . '>' . "\r\n";
+
+mail($ini['request_email'], $mail_title . ' ' . date('d-m-Y H:i:s'), $mail, $headers);
+
